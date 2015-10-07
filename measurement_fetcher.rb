391282b9de1ff9f7bb101cbd221536208efaa63d
tomek <tomek.bartynski@gmail.com>
2015-10-07 08:48:34 +0200

    extracting methods from get method

--- measurement_fetcher.rb
+++ measurement_fetcher.rb
@@ -9,69 +9,88 @@ class MeasurementFetcher
     @conn = Faraday.new(url: DAP_BASE_URL, ssl:{verify: false})
   end
 
-  def get(ctx_id, device_aggregation_custom_id, from, to)
-    da_resp = @conn.get(
-      "/api/v1/device_aggregations?custom_id=#{device_aggregation_custom_id}",
-      { private_token: PRIVATE_TOKEN }
-    ).body
-    da = JSON.parse(da_resp)
-    da_id = da['device_aggregations'].first['id']
+  def devices(ids)
     devices_resp = @conn.get(
-      "/api/v1/devices?device_aggregation_id=#{da_id}",
-      { private_token: PRIVATE_TOKEN }
+        "/api/v1/devices?device_aggregation_id=#{da_id}",
+        { private_token: private_token }
     ).body
-    devices = JSON.parse(devices_resp)['devices']
-    parameter_ids = devices.collect { |d| d['parameter_ids'] }.flatten
+    JSON.parse(devices_resp)['devices']
+  end
+
+  def get(ctx_id, profile_id, from, to)
+    device_aggregations = device_aggregations_for_profile(profile_id)
+    device_aggregations.each do |da|
+      devices = devices(da['device_ids'])
+      parameter_ids = devices.collect { |d| d['parameter_ids'] }.flatten
+      parameters = parameters(parameter_ids)
+
+      temp_param_id = parameters.select do |p|
+        p['measurement_type_name'] == 'Temperatura'
+      end.first['id']
+
+      press_param_id = parameters.select do |p|
+        p['measurement_type_name'] == 'Ciśnienie porowe'
+      end.first['id']
+
+      temp_tl_id = timeline(ctx_id, temp_param_id)['id']
+
+      press_tl_id = timeline(ctx_id, press_param_id)['id']
+
+      temp_measurements = temperature_measurements(temp_tl_id, from, to)
+
+      press_measurements = pressure_measurements(press_tl_id, from, to)
+
+      puts "Temperature measurements:\n #{temp_measurements}"
+      puts "Pressure measurements:\n #{press_measurements}"
+    end
+  end
+
+  def parameters(parameter_ids)
     parameters_resp = @conn.get(
-      "/api/v1/parameters?id=#{parameter_ids.join(',')}",
-      { private_token: PRIVATE_TOKEN }
+        "/api/v1/parameters?id=#{parameter_ids.join(',')}",
+        {private_token: private_token}
     ).body
-    parameters = JSON.parse(parameters_resp)['parameters']
-    temp_param_id = parameters.select do |p|
-      p['measurement_type_name'] == 'Temperatura'
-    end.first['id']
+    JSON.parse(parameters_resp)['parameters']
+  end
 
-    press_param_id = parameters.select do |p|
-      p['measurement_type_name'] == 'Ciśnienie porowe'
-    end.first['id']
-
-    tt_resp = @conn.get(
-      "/api/v1/timelines?parameter_id=#{temp_param_id}&context_id=#{ctx_id}",
-      { private_token: PRIVATE_TOKEN }
-    ).body
-    temp_tl_id = JSON.parse(tt_resp)['timelines'].first['id']
-
+  def timeline(ctx_id, parameter_id)
     pt_resp = @conn.get(
-      "/api/v1/timelines?parameter_id=#{press_param_id}&context_id=#{ctx_id}",
-      { private_token: PRIVATE_TOKEN }
+        "/api/v1/timelines?parameter_id=#{parameter_id}&context_id=#{ctx_id}",
+        {private_token: private_token}
     ).body
-    press_tl_id = JSON.parse(pt_resp)['timelines'].first['id']
+    JSON.parse(pt_resp)['timelines'].first
+  end
 
+  def temperature_measurements(timeline_id, from, to)
     temp_measurements_resp = @conn.get(
-        "/api/v1/measurements?timeline_id=#{temp_tl_id}"\
+        "/api/v1/measurements?timeline_id=#{timeline_id}"\
         "&time_from=#{from}&time_to=#{to}",
-        { private_token: PRIVATE_TOKEN }
+        {private_token: private_token}
     ).body
-    temp_measurements = JSON.parse(temp_measurements_resp)['measurements']
+    JSON.parse(temp_measurements_resp)['measurements']
+  end
 
+  def pressure_measurements(timeline_id, from, to)
     press_measurements_resp = @conn.get(
-        "/api/v1/measurements?timeline_id=#{press_tl_id}"\
+        "/api/v1/measurements?timeline_id=#{timeline_id}"\
         "&time_from=#{from}&time_to=#{to}",
-        { private_token: PRIVATE_TOKEN }
+        {private_token: private_token}
     ).body
-    press_measurements = JSON.parse(press_measurements_resp)['measurements']
-
-    puts "Temperature measurements:\n #{temp_measurements}"
-    puts "Pressure measurements:\n #{press_measurements}"
+    JSON.parse(press_measurements_resp)['measurements']
   end
 
   def device_aggregations_for_profile(profile_id)
     da_resp = @conn.get(
       "/api/v1/device_aggregations?profile_id=#{profile_id}",
-      { private_token: PRIVATE_TOKEN }
+      { private_token: private_token }
     ).body
     das = JSON.parse(da_resp)['device_aggregations']
     puts "Device aggregations:\n#{das}"
+  end
+
+  private
+  def private_token
+    ENV['PRIVATE_TOKEN'] || PRIVATE_TOKEN
   end
 
 end
@@ -79,7 +98,7 @@ end
 
 
 fetcher = MeasurementFetcher.new
-time_from='2015-10-03 9:40:40 +0200'
-time_to='2015-10-03 9:45:40 +0200'
+#time_from = '2015-10-03 9:40:40 +0200'
+#time_to = '2015-10-03 9:45:40 +0200'
 #fetcher.get(1, 'UT7', time_from, time_to)
 fetcher.device_aggregations_for_profile(8)
