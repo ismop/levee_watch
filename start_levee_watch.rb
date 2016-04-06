8381507e567eaf11255e797649ac4f96932acd9a
tomek <tomek.bartynski@gmail.com>
2016-04-06 15:42:12 +0200

    refactored scheduling run workflow job and removed scheduling checking threat assessment job

--- start_levee_watch.rb
+++ start_levee_watch.rb
@@ -6,25 +6,18 @@ Resque::Scheduler.dynamic = true
 
 def run_workflow
   name = 'run_workflow'
+  hypgen_params = {
+      threat_assessment:
+          {
+              name: 'testing levee watch',
+              section_ids:[1,2]
+          }
+  }
   config = {
       class: 'RunWorkflow',
       every: [WORKFLOW_RUN_PERIOD, {first_in: FIRST_WORKFLOW_RUN_DELAY}],
       persist: false,
-      args: {a1: 'A', a2: 'B'},
-      queue: :job
-  }
-  Resque.set_schedule name, config
-end
-
-def check_threat_assessment
-  name = 'check_threat_assessment'
-  config = {
-      class: 'CheckThreatAssessment',
-      every: [
-          CHECK_THREAT_ASSESSMENT_PERIOD,
-          {first_in: FIRST_CHECK_THREAT_ASSESSMENT_DELAY}
-      ],
-      persist: false,
+      args: {hypgen_params: hypgen_params},
       queue: :job
   }
   Resque.set_schedule name, config
@@ -48,9 +41,7 @@ $log.info 'Starting Levee watch'
 $log.info 'Scheduling Hypgen workflow'
 run_workflow
 $log.info 'Workflow scheduled'
-check_threat_assessment
-$log.info 'Checking threat assessment scheduled'
-$log.info 'Scheduling workflow monitoring'
-monitor_workflow
-$log.info 'Scheduled workflow monitoring'
+# $log.info 'Scheduling workflow monitoring'
+# monitor_workflow
+# $log.info 'Scheduled workflow monitoring'
 $log.info 'Work done.'
