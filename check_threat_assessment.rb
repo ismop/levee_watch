64e656574fa8119e958cc257c852e46f27fe4872
tomek <tomek.bartynski@gmail.com>
2016-04-13 12:23:44 +0200

    implemented logic to reschedule wf based on threat level

--- check_threat_assessment.rb
+++ check_threat_assessment.rb
@@ -1,6 +1,10 @@
 require_relative './config.rb'
+require_relative './helpers.rb'
 
 require 'resque-scheduler'
+require 'faraday'
+
+Resque::Scheduler.dynamic = true
 
 class CheckThreatAssessment
   @queue = :job
@@ -12,7 +16,24 @@ class CheckThreatAssessment
         "/api/v1/threat_assessments/#{params['threat_assessment_id']}",
         {private_token: PRIVATE_TOKEN}
     )
-    $log.info "Response status: #{resp.status}"
-    $log.info "Response body: #{resp.body}"
+    results = results(resp)
+    wf_schedule = Resque.all_schedules['run_workflow']
+    wf_schedule['every'] =
+        run_wf_job_every_setting(assess_threat_level(results))
+    Resque.set_schedule 'run_workflow', wf_schedule
+    $log.info "Rescheduled running WF (every #{wf_schedule['every']})"
+  end
+
+  private
+  def self.results(dap_resp)
+    JSON.parse(dap_resp.body)['threat_assessment']['results']
+  end
+
+  # Returns:
+  # - 0 if there is no result with similarity > 0.5
+  # - 1 otherwise
+  def self.assess_threat_level(results)
+    count = results.count {|r| r['similarity']> SIMILARITY_THRESHOLD}
+    count > 0 ? 1 : 0
   end
 end
